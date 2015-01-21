e45ee5bf7ad99b8319abe70a9f394d881478f85a
Zamboni91 <marc.manzoni@t-online.de>
2015-01-21 22:20:34 +0100

    Ausrichtung der Axen in DOCS

--- dosenschuttler_4.4/src/main/java/com/example/marc/dosenschuttler_44/moveMe/moveCounter.java
+++ dosenschuttler_4.4/src/main/java/com/example/marc/dosenschuttler_44/moveMe/moveCounter.java
@@ -1,8 +1,6 @@
 package com.example.marc.dosenschuttler_44.moveMe;
 
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorManager;
 
 /**
  * Created by Marc on 20.01.2015.
@@ -10,20 +8,17 @@ import android.hardware.SensorManager;
 public class moveCounter {
     private int count;
     //If the device lies flat on the table the movement to the right is positive in the X-Axis
-    private Sensor motionX;
+    private int motionX;
 
-    private Sensor motionY;
-    private Sensor motionZ;
+    private int motionY;
+    private int motionZ;
 
-    public moveCounter(SensorEvent event){
+    public moveCounter(){
         this.count = 0;
-/*
-        motionX = Sensor.TYPE_LINEAR_ACCELERATION.values[0];
-        motionY = Sensor.TYPE_LINEAR_ACCELERATION.values[1];
-        motionZ = Sensor.TYPE_LINEAR_ACCELERATION.values[2];
 
-        SensorEvent.
-*/
+        motionX = Sensor.TYPE_LINEAR_ACCELERATION;
+
+
     }
 
 
